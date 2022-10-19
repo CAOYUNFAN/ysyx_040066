52ff07c99bcf5b0b8bb206079be2cde8a83491b8
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-20 05:48:03 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:48:03 up 4 min,  1 user,  load average: 2.23, 1.48, 0.64

--- npc/csrc/src/dpi-c/dpi-c.cpp
+++ npc/csrc/src/dpi-c/dpi-c.cpp
@@ -23,7 +23,7 @@ extern "C" void status_now(unsigned status){
     memcpy(&cpu_status,&status,1);
 }
 
-extern "C" void raise_timer_intr(uLL NO,uLL pc){
+extern "C" void raise_intr_timer(uLL NO,uLL pc){
     intr_NO=NO;intr_pc=pc;intr_is_jmp=1;
 }
 
