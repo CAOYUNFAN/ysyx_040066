1bf0d3e108a5a4b06ce1a81f29cf67f9bf093ae2
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-22 05:49:12 +0800

    >  sim RTL ysyx_220066 曹云帆 Linux cao-VirtualBox 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:49:12 up  6:52,  1 user,  load average: 1.61, 1.06, 0.72

--- npc/vsrc/CPU/Multi/ysyx_040066_walloc_33bits.v
+++ npc/vsrc/CPU/Multi/ysyx_040066_walloc_33bits.v
@@ -19,64 +19,65 @@ module ysyx_040066_walloc_33bits(
     output cout,s
 );
     //first
-    wire [10:0] first_s;
-    ysyx_040066_csa csa00(.src(src_in[32:30]),.cout(cout_group1[10]),.s(first_s[10]));
-    ysyx_040066_csa csa01(.src(src_in[29:27]),.cout(cout_group1[ 9]),.s(first_s[ 9]));
-    ysyx_040066_csa csa02(.src(src_in[26:24]),.cout(cout_group1[ 8]),.s(first_s[ 8]));
-    ysyx_040066_csa csa03(.src(src_in[23:21]),.cout(cout_group1[ 7]),.s(first_s[ 7]));
-    ysyx_040066_csa csa04(.src(src_in[20:18]),.cout(cout_group1[ 6]),.s(first_s[ 6]));
-    ysyx_040066_csa csa05(.src(src_in[17:15]),.cout(cout_group1[ 5]),.s(first_s[ 5]));
-    ysyx_040066_csa csa06(.src(src_in[14:12]),.cout(cout_group1[ 4]),.s(first_s[ 4]));
-    ysyx_040066_csa csa07(.src(src_in[11: 9]),.cout(cout_group1[ 3]),.s(first_s[ 3]));
-    ysyx_040066_csa csa08(.src(src_in[ 8: 6]),.cout(cout_group1[ 2]),.s(first_s[ 2]));
-    ysyx_040066_csa csa09(.src(src_in[ 5: 3]),.cout(cout_group1[ 1]),.s(first_s[ 1]));
-    ysyx_040066_csa csa0a(.src(src_in[ 2: 0]),.cout(cout_group1[ 0]),.s(first_s[ 0]));
+    wire [10:0] s_1;
+    wire [10:0] part1 [2:0];
+    assign part1[0]=src_in[32:22];
+    assign part1[1]=src_in[21:11];
+    assign part1[2]=src_in[10:0];
+    assign cout_group1=(part1[0]&part1[1])|(part1[0]&part1[2])|(part1[1]&part1[2]);
+    assign s_1=part1[0]^part1[1]^part1[2];
 
     //second
-    wire [6:0] second_s;
-    ysyx_040066_csa csa10(.src(first_s[10: 8]),.cout(cout_group2[ 6]),.s(second_s[6]));
-    ysyx_040066_csa csa11(.src(first_s[ 7: 5]),.cout(cout_group2[ 5]),.s(second_s[5]));
-    ysyx_040066_csa csa12(.src(first_s[ 4: 2]),.cout(cout_group2[ 4]),.s(second_s[4]));
-    ysyx_040066_csa csa13(.src({first_s[1:0],cin1[10]}),.cout(cout_group2[ 3]),.s(second_s[3]));
-    ysyx_040066_csa csa14(.src(cin1[ 9: 7]),.cout(cout_group2[ 2]),.s(second_s[2]));
-    ysyx_040066_csa csa15(.src(cin1[ 6: 4]),.cout(cout_group2[ 1]),.s(second_s[1]));
-    ysyx_040066_csa csa16(.src(cin1[ 3: 1]),.cout(cout_group2[ 0]),.s(second_s[0]));
+    wire [6:0] s_2;
+    wire [6:0] part2 [2:0];
+    assign part2[0]=s_1[10:4];
+    assign part2[1]={s_1[3:0],cin1[10:8]};
+    assign part2[2]={cin1[7:1]};
+    assign cout_group2=(part2[0]&part2[1])|(part2[0]&part2[2])|(part2[1]&part2[2]);
+    assign s_2=part2[0]^part2[1]^part2[2];
 
     //third
-    wire [4:0] third_s;
-    ysyx_040066_csa csa20(.src(second_s[6:4]),.cout(cout_group3[4]),.s(third_s[4]));
-    ysyx_040066_csa csa21(.src(second_s[3:1]),.cout(cout_group3[3]),.s(third_s[3]));
-    ysyx_040066_csa csa22(.src({second_s[0],cin1[0],cin2[6]}),.cout(cout_group3[2]),.s(third_s[2]));
-    ysyx_040066_csa csa23(.src(cin2[5:3]),.cout(cout_group3[1]),.s(third_s[1]));
-    ysyx_040066_csa csa24(.src(cin2[2:0]),.cout(cout_group3[0]),.s(third_s[0]));
+    wire [4:0] s_3;
+    wire [4:0] part3 [2:0];
+    assign part3[0]=s_2[6:2];
+    assign part3[1]={s_2[1:0],cin1[0],cin2[6:5]};
+    assign part3[2]=cin2[4:0];
+    assign cout_group3=(part3[0]&part3[1])|(part3[0]&part3[2])|(part3[1]&part3[2]);
+    assign s_3=part3[0]^part3[1]^part3[2];
 
     //fourth
-    wire [2:0] fourth_s;
-    ysyx_040066_csa csa30(.src(third_s[4:2]),.cout(cout_group4[2]),.s(fourth_s[2]));
-    ysyx_040066_csa csa31(.src({third_s[1:0],cin3[4]}),.cout(cout_group4[1]),.s(fourth_s[1]));
-    ysyx_040066_csa csa32(.src(cin3[3:1]),.cout(cout_group4[0]),.s(fourth_s[0]));
+    wire [2:0] s_4;
+    wire [2:0] part4 [2:0];
+    assign part4[0]=s_3[4:2];
+    assign part4[1]={s_3[1:0],cin3[4]};
+    assign part4[2]=cin3[3:1];
+    assign cout_group4=(part4[0]&part4[1])|(part4[0]&part4[2])|(part4[1]&part4[2]);
+    assign s_4=part4[0]^part4[1]^part4[2];
 
     //fifth
-    wire [1:0] fifth_s;
-    ysyx_040066_csa csa40(.src(fourth_s[2:0]),.cout(cout_group5[1]),.s(fifth_s[1]));
-    ysyx_040066_csa csa41(.src({cin3[0],cin4[2:1]}),.cout(cout_group5[0]),.s(fifth_s[0]));
+    wire [1:0] s_5;
+    wire [1:0] part5 [2:0];
+    assign part5[0]=s_4[2:1];
+    assign part5[1]={s_4[0],cin3[0]};
+    assign part5[2]={cin4[2:1]};
+    assign cout_group5=(part5[0]&part5[1])|(part5[0]&part5[2])|(part5[1]&part5[2]);
+    assign s_5=part5[0]^part5[1]^part5[2];
 
     //sixth
-    wire sixth_s;
-    ysyx_040066_csa csa50(.src({fifth_s,cin4[0]}),.cout(cout_group6),.s(sixth_s));
+    wire s_6;
+    ysyx_040066_csa csa50(.src({s_5,cin4[0]}),.cout(cout_group6),.s(s_6));
 
     //seventh
-    wire seventh_s;
-    ysyx_040066_csa csa60(.src({sixth_s,cin5[1:0]}),.cout(cout_group7),.s(seventh_s));
+    wire s_7;
+    ysyx_040066_csa csa60(.src({s_6,cin5[1:0]}),.cout(cout_group7),.s(s_7));
 
     //final
-    ysyx_040066_csa csa70(.src({seventh_s,cin6,cin7}),.cout(cout),.s(s));
+    ysyx_040066_csa csa70(.src({s_7,cin6,cin7}),.cout(cout),.s(s));
 endmodule
 
 module ysyx_040066_csa(
     input [2:0] src,
     output cout,s
 );
-    assign s=src[0]^src[1]^src[2];
-    assign cout=(src[0]&&src[1])||(src[0]&&src[2])||(src[1]&&src[2]);
+    assign {s,cout}={1'b0,src[0]}+{1'b0,src[1]}+{1'b0,src[2]};
 endmodule
